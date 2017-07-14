5f9d5e51f160fa5644fc04450ba94ece40928d59
kmazi <ugochukwu.mazi@andela.com>
2017-07-14 05:39:44 +0100

    chore(heroku): Push to heroku - Prepare app to push to heroku - Delivers #146858771

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,4 +1,3 @@
-import path from 'path';
 import { createDocument,
          getAllDocuments,
          findDocument,
@@ -21,51 +20,31 @@ import { signUpUser,
  * @return {null} returns void
  */
 const routes = (router) => {
-  // setup path for serving static files
-  const sourcePath = path.join(__dirname, '../../client/');
   // route to create a new user
   router.post('/users', signUpValidation, signUpUser);
   // route to signin a user
   router.post('/users/login', signInValidation, signInUser);
 
   // route to get all users and paginate them
-  router.get('/users', getAllUsers);
+  router.get('/users', verifyToken, getAllUsers);
   // Find a specific user
-  router.get('/users/:id', findUser);
+  router.get('/users/:id', verifyToken, findUser);
   // Update a specific user
-  router.put('/users/:id', signUpValidation, updateUser);
+  router.put('/users/:id', verifyToken, signUpValidation, updateUser);
   // route to fetch documents belonging to a user
-  router.get('/users/:id/documents', getUserDocuments);
+  router.get('/users/:id/documents', verifyToken, getUserDocuments);
   // Deletes a specific user
-  router.delete('/users/:id', deleteUser);
+  router.delete('/users/:id', verifyToken, deleteUser);
   // route to search for users
-  router.get('/search/users', findUsers);
+  router.get('/search/users', verifyToken, findUsers);
   // route to get all documents
-  router.get('/documents', getAllDocuments);
+  router.get('/documents', verifyToken, getAllDocuments);
   // route to create a new document
-  router.post('/documents', createDocument);
+  router.post('/documents', verifyToken, createDocument);
   // route to find a specific document
-  router.get('/documents/:id', findDocument);
+  router.get('/documents/:id', verifyToken, findDocument);
   // route to delete a specific document
-  router.delete('/documents/:id', deleteDocument);
-  // route that serves the home page
-  // router.get('/*', (req, res) => res.sendFile(`${sourcePath}index.html`));
-  // router.get('/*', (req, res, next) => {
-  //   const filename = path.join(sourcePath, 'index.html');
-  //   compiler.outputFileSystem.readFile(filename, (err, result) => {
-  //     if (err) {
-  //       return next(err);
-  //     }
-  //     res.set('content-type', 'text/html');
-  //     res.send(result);
-  //     res.end();
-  //   });
-  // });
-// // Default route when ther is no match
-//   router.get('*', (req, res) => {
-//     res.status(404).send(`404 error! The page you're 
-//       searching for cannot be found`);
-//   });
+  router.delete('/documents/:id', verifyToken, deleteDocument);
 };
 
 export default routes;
